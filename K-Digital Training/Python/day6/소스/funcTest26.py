560f6fd0bb00bcc70cbd4cd9da59d61892ed0d6e
Jude Kim <kjh890902@gmail.com>
2021-02-04 23:05:16 +0900

    day6_2

---
+++ K-Digital Training/Python/day6/소스/funcTest26.py
@@ -0,0 +1,12 @@
+#  파이썬 정복 7장 함수 (157p)
+def calcscore(name, *score, **option):
+    print(name)
+    sum = 0
+    for s in score:
+        sum += s
+    print("총점 :", sum)
+    if (option['avg'] == True ):
+        print("평균 :", sum / len(score))
+
+calcscore("김상형", 88, 99, 77, avg = True)
+calcscore("김한슬", 99, 98, 95, 89, avg = False)
