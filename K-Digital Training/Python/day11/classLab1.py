e8e25c0201d93b74aa2b3c34f1962017e9525910
Jude Kim <kjh890902@gmail.com>
2021-02-25 09:22:37 +0900

    day11_7

---
+++ K-Digital Training/Python/day11/classLab1.py
@@ -0,0 +1,16 @@
+class Member:
+    def __init__(self):
+        self.name = None
+        self.account = None
+        self.passwd = None
+        self.birthyear = None
+
+m1 = Member()
+m2 = Member()
+m3 = Member()
+
+m1.name = "차주혁"
+m1.account = "jhcha123"
+m1.passwd = 1234
+m1.birthyear = 1985
+
